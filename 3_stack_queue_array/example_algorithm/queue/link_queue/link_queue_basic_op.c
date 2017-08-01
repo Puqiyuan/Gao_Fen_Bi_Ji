a4d50df119a09725532a02129eb096a1eb634690
Qiyuan Pu <pqy7172@gmail.com>
2017-08-01 16:17:17 +0800

    实现元素入链队算法。

--- 3_stack_queue_array/example_algorithm/queue/link_queue/link_queue_basic_op.c
+++ 3_stack_queue_array/example_algorithm/queue/link_queue/link_queue_basic_op.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 typedef struct QNode
@@ -33,6 +34,25 @@ int QueueEmpty(LiQueue *lqu)
 }
 
 
+int enQueue(LiQueue *lqu, int x)
+{
+	QNode *p = (QNode*)malloc(sizeof(QNode));
+
+	p -> data = x;
+	p -> next = NULL;
+
+	if (lqu -> rear == NULL || lqu -> front == NULL)
+		lqu -> front = lqu -> rear = p;
+	else
+		{
+			lqu -> rear -> next = p;
+			lqu -> rear = p;
+		}
+
+	return 1;
+}
+
+
 int main(int argc, char *argv[])
 {
 	LiQueue lqu;
@@ -49,7 +69,14 @@ int main(int argc, char *argv[])
 		printf("The queue is empty.\n\n");
 	else
 		printf("The queue is not empty.\n\n");
+
+	int x[3] = {-1, 0, 99}, i;
+	for (i = 0; i <= 2; i++)
+		{
+			enQueue(&lqu, x[i]);
+			printf("The element %d enQueue: OK.\n", x[i]);
+		}
+	printf("\n");
 	
 	return 0;
 }
-
