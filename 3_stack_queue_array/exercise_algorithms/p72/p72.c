6bb877d41560d9ed331f44d032773d59efa7cd3a
Qiyuan Pu <pqy7172@gmail.com>
2017-08-04 15:42:55 +0800

    实现enqueue和pop算法。

--- 3_stack_queue_array/exercise_algorithms/p72/p72.c
+++ 3_stack_queue_array/exercise_algorithms/p72/p72.c
@@ -18,9 +18,9 @@ int initStack(SqStack *st)
 }
 
 
-int isEmpty(SqStack st)
+int isEmpty(SqStack *st)
 {
-        if (st.top == -1)
+        if (st -> top == -1)
                 return 1;
 
         return 0;
@@ -40,3 +40,66 @@ int push(SqStack *st, int x)
 }
 
 
+int pop(SqStack *st, int *x)
+{
+	if (st -> top == -1)
+		return 0;
+
+	*x = st -> data[st -> top];
+	--(st -> top);
+
+	return 1;
+}
+
+
+int enqueue(SqStack *s1, SqStack *s2, int x)
+{
+	int *y = (int*)malloc(sizeof(int));
+
+
+	if (s1 -> top == maxSize - 1)
+		{
+			if (!isEmpty(s2))
+				return 0;
+
+			else if (isEmpty(s2))
+				{
+					while (!isEmpty(s1))
+						{
+							pop(s1, y);
+							push(s2, *y);
+						}
+					push(s1, x);
+					return 1;
+				}
+		}
+
+	else
+		push(s1, x);
+
+	return 1;
+}
+
+
+int main(int argc, char *argv[])
+{
+	SqStack s1, s2;
+	
+	initStack(&s1);
+	initStack(&s2);
+	
+	int x[11] = {2, 4, 1, 0, -1, 99, 101, -12, 3, 78, 999}, i;
+
+	for (i = 0; i <= 10; i++)
+		{
+			if (enqueue(&s1, &s2, x[i]) == 1)
+				printf("Enqueue element %d: OK, value: %d.\n",
+				       i, x[i]);
+
+						else
+				printf("Enqueue element %d: Failed, value: %d.\n",
+				       i, x[i]);
+		}
+	
+	return 0;
+}
