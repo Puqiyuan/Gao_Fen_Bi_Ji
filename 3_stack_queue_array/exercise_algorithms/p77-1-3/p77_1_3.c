f8b5fc3a081b320966be0ac59ae47167a0e01790
Qiyuan Pu <pqy7172@gmail.com>
2017-08-15 21:32:22 +0800

    添加测试结果。

--- 3_stack_queue_array/exercise_algorithms/p77-1-3/p77_1_3.c
+++ 3_stack_queue_array/exercise_algorithms/p77-1-3/p77_1_3.c
@@ -1,3 +1,18 @@
+/* 
+   Test result:
+   pqy7172@debian:~/.../p77-1-3$ make run 
+   gcc -Wall p77_1_3.c -o test
+   ./test
+   IO
+   The operation sequence is legal.
+   pqy7172@debian:~/.../p77-1-3$ make run 
+   ./test
+   OI
+   The operation sequence is NOT legal.
+   pqy7172@debian:~/.../p77-1-3$ 
+   [pqy7172@debian:0] 0:ss-local 1:bash                               0.20 100% Tue 2017-08-15 21:31
+ */
+
 #include <stdio.h>
 
 int Judge(char ch[])
