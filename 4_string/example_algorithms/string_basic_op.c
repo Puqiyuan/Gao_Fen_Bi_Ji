b24be275c8af0c07f9551700e819ab0c676743d1
Qiyuan Pu <pqy7172@gmail.com>
2018-10-06 20:56:00 +0800

    complete of basic operations of string

--- 4_string/example_algorithms/string_basic_op.c
+++ 4_string/example_algorithms/string_basic_op.c
@@ -56,13 +56,125 @@ int strassign(Str** str, char* ch)
 		}
 }
 
+int strlength(Str str)
+{
+	return str.length;
+}
+
+int strcompare(Str s1, Str s2)
+{
+	for (int i = 0; i < s1.length && i < s2.length; ++i)
+		if (s1.ch[i] != s2.ch[i])
+			return s1.ch[i] - s2.ch[i];
+	return s1.length - s2.length;
+}
+
+int concat(Str** str, Str str1, Str str2)
+{
+	if ((*str)->ch)
+		{
+			free((*str) -> ch);
+			(*str) -> ch = NULL;
+		}
+	(*str) -> ch = (char*)malloc(sizeof(char) * (str1.length + str2.length+1));
+	if ((*str) -> ch == NULL)
+		return 0;
+	int i = 0;
+	while (i < str1.length)
+		{
+			(*str) -> ch[i] = str1.ch[i];
+			++i;
+		}
+	int j = 0;
+	while(j <= str2.length)
+		{
+			(*str) -> ch[i+j] = str2.ch[j];
+			++j;
+		}
+	(*str) -> length = str1.length + str2.length;
+	return 1;
+}
+
+int substring(Str** substr, Str str, int pos, int len)
+{
+	int LEN = len;
+	if (pos < 0 || pos >= str.length || len < 0 || len > str.length - pos)
+		return 0;
+
+	if ((*substr) -> ch)
+		{
+			free((*substr) -> ch);
+			(*substr) -> ch = NULL;
+		}
+
+	if (len = 0)
+		{
+			(*substr) -> ch = NULL;
+			(*substr) -> length = 0;
+			return 1;
+		}
+	else
+		{
+			// printf("len126: %d\n", len), until here len is 0, so why len become to 0?
+			((*substr) -> ch) = (char*)malloc(sizeof(char) * len + 1);
+			int i = pos;
+			int j = 0;
+			while(i < pos + LEN)
+				{
+					(*substr) -> ch[j] = str.ch[i];
+					++i;
+					++j;
+				}
+			(*substr) -> ch[j] = '\0';
+			(*substr) -> length = len;
+			return 1;
+		}
+}
+
+int clearstring(Str** str)
+{
+	if ((*str) -> ch)
+		{
+			free((*str) -> ch);
+			(*str) -> ch = NULL;
+		}
+	(*str) -> length = 0;
+	return 1;
+}
+
 int main(int argc, char *argv[])
 {
-	Str* str = (Str*)malloc(sizeof(Str));
 
-	printf("test for strassign() function:\n");
-	strassign(&str, "this is a assign string.");
-	printf("%s\n", *str);
+	printf("1. test for strassign() function:\n");
+	Str* str = (Str*)malloc(sizeof(Str));
+	strassign(&str, "this is a assign string");
+	printf("%s\n\n", *str);
+
+	printf("2. test for strlength() function:\n");
+	printf("the testing string is: '%s', it's length is %d\n\n", *str, strlength(*str));
+
+	printf("3. test for strcompare() function:\n");
+	Str* st1 = (Str*)malloc(sizeof(Str));
+	Str* st2 = (Str*)malloc(sizeof(Str));
+	strassign(&st1, "this is string1");
+	strassign(&st2, "this is string2 this is string2");
+	printf("st1: %s\n", *st1);
+	printf("st2: %s\n", *st2);
+	printf("the compare result of st1 and st2: %d\n\n", strcompare(*st1, *st2));
+
+	printf("4. test for concat() function:\n");
+	Str* str4 = (Str*)malloc(sizeof(str));
+	concat(&str4, *st1, *st2);
+	printf("after concat of st1 and st2: %s\n\n", *str4);
+
+	printf("5. test for substring() function:\n");
+	Str* substr = (Str*)malloc(sizeof(Str));
+	substring(&substr, *st2, 2, 8);
+	printf("get 8 chars after third char of st2: %s\n\n", *substr);
+
+	printf("6. test for clearstring() function:\n");
+	clearstring(&st2);
+	printf("after free of st2: %s\n", *st2);
 	
 	return 0;
 }
